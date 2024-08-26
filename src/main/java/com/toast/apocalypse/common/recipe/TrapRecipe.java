68d27af5e9739e36e25b63d6718beb96986dc733
Sarinsa <unconfigured@null.spigotmc.org>
2024-08-26 15:15:31 +0200

    Made TrapRecipe return ItemStack.EMPTY as result item

--- src/main/java/com/toast/apocalypse/common/recipe/TrapRecipe.java
+++ src/main/java/com/toast/apocalypse/common/recipe/TrapRecipe.java
@@ -96,7 +96,7 @@ public final class TrapRecipe implements Recipe<DynamicTrapBlockEntity> {
 
     @Override
     public ItemStack getResultItem(RegistryAccess registryAccess) {
-        return null;
+        return ItemStack.EMPTY;
     }
 
     @Nonnull
