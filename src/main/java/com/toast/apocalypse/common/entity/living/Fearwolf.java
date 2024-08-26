559574be6a1b44c2bc9930832c4250469b771ad6
Sarinsa <unconfigured@null.spigotmc.org>
2024-08-26 17:21:04 +0200

    Lowered Fearwolf movement speed a tad

--- src/main/java/com/toast/apocalypse/common/entity/living/Fearwolf.java
+++ src/main/java/com/toast/apocalypse/common/entity/living/Fearwolf.java
@@ -48,8 +48,8 @@ public class Fearwolf extends Monster implements Enemy {
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(2, new FearwolfRunAwayGoal(this, 1.0D));
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, true));
-        this.goalSelector.addGoal(3, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 0.85D, true));
+        this.goalSelector.addGoal(3, new WaterAvoidingRandomStrollGoal(this, 0.6D));
         this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers());
